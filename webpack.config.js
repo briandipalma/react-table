e0f0f3f1dc4f0e003155bd245d68ab10c2a8b527
Brian Di Palma <offler@gmail.com>
2015-02-01 16:28:19 +0000

    Add 6to5 loader configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
+'use strict';
+
 module.exports = {
-    entry: "./src/entry.js",
-    output: {
-        path: "__build__",
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    }
+	entry: './src/entry.js',
+	output: {
+		path: '__build__',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{test: /\.js$/, exclude: /node_modules/, loader: '6to5-loader'}
+		]
+	}
 };
